90041cf1b2bde4ad86993cfbeb4984e12f72f8d0
Vinipontess <pontesivini@gmail.com>
2025-09-11 14:05:22 -0300

    Adicionando o tp01 - Vinicius Pontes e Eduardo Barbosa

---
+++ README.md
@@ -0,0 +1,8 @@
+## Trabalhos práticos de PRDM
+
+**Aluno:** Vinícius Pontes Oliva - CB3025764
+**Aluno:** Eduardo Barbosa - CB302637X
+
+Este repositório é exclusivamente para as atividades de programção para dispositivos móveis
+
+**Linguagem**: C# utilizando o MAUI
---
+++ TP1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace TP1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+     <summary>
+     </summary>
+	private void OnOkButtonClicked(object sender, EventArgs e)
+    {
+        if (IdEntry.Text == "admin" && PassEntry.Text == "senha@dmin")
+        {
+            DisplayAlert("Login", "Logou com sucesso", "OK");
+        }
+        else
+        {
+            DisplayAlert("Login", "Login inválido, não autorizado", "OK");
+        }
+    }
+
+     <summary>
+     </summary>
+	private void OnLimparButtonClicked(object sender, EventArgs e)
+    {
+        IdEntry.Text = string.Empty;
+        PassEntry.Text = string.Empty;
+
+        IdEntry.Focus();
+    }
+
+     <summary>
+     </summary> 
+	private async void OnCreditosButtonClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Créditos", "Desenvolvido por:\n\n- Vinícius Pontes\n- Eduardo Barbosa", "Fechar");
+    }
+}
